0c668492ead1bc4f3a129028343e6a6f456953ca
Temitope Shobayo <shobayomykel@gmail.com>
2024-01-12 20:33:57 +0100

    Finally done

--- js/script.js
+++ js/script.js
@@ -14,3 +14,172 @@ function closeSidebar() {
         sidebarOpen = false;
     }
 }
+
+
+// --------------------------CHARTS---------------------
+
+var barChartOptions = {
+    series: [{
+        data: [10, 8, 6, 4, 2]
+    }],
+    chart: {
+        type: 'bar',
+        height: 350,
+        toolbar: {
+            show: false
+        },
+    },
+    colors: [
+        "#246dec",
+        "#cc3c43",
+        "#367952",
+        "#f5b74f",
+        "#4f35a1"
+    ],
+    plotOptions: {
+        bar: {
+            distributed: true,
+            borderRadius: 4,
+            horizontal: false,
+            columunWidth: '40%'
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    xaxis: {
+        categories: ['Laptop', 'Phone', 'Monitor', 'Headphones', 'Camera'],
+    },
+    yaxis: {
+        title: {
+            text: "Count"
+        }
+    }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+/////////////////////////////////////////Chart 2
+
+const areaChartOptions = {
+    series: [
+        {
+            name: 'Purchase Orders',
+            data: [31, 40, 28, 51, 42, 109, 100],
+        },
+        {
+            name: 'Sales Orders',
+            data: [11, 32, 45, 32, 34, 52, 41],
+        },
+    ],
+    chart: {
+        type: 'area',
+        background: 'transparent',
+        height: 350,
+        stacked: false,
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: ['#00ab57', '#d50000'],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    dataLabels: {
+        enabled: false,
+    },
+    fill: {
+        gradient: {
+            opacityFrom: 0.4,
+            opacityTo: 0.1,
+            shadeIntensity: 1,
+            stops: [0, 100],
+            type: 'vertical',
+        },
+        type: 'gradient',
+    },
+    grid: {
+        borderColor: '#55596e',
+        yaxis: {
+            lines: {
+                show: true,
+            },
+        },
+        xaxis: {
+            lines: {
+                show: true,
+            },
+        },
+    },
+    legend: {
+        labels: {
+            colors: '#f5f7ff',
+        },
+        show: true,
+        position: 'top',
+    },
+    markers: {
+        size: 6,
+        strokeColors: '#1b2635',
+        strokeWidth: 3,
+    },
+    stroke: {
+        curve: 'smooth',
+    },
+    xaxis: {
+        axisBorder: {
+            color: '#55596e',
+            show: true,
+        },
+        axisTicks: {
+            color: '#55596e',
+            show: true,
+        },
+        labels: {
+            offsetY: 5,
+            style: {
+                colors: '#f5f7ff',
+            },
+        },
+    },
+    yaxis: [
+        {
+            title: {
+                text: 'Purchase Orders',
+                style: {
+                    color: '#f5f7ff',
+                },
+            },
+            labels: {
+                style: {
+                    colors: ['#f5f7ff'],
+                },
+            },
+        },
+        {
+            opposite: true,
+            title: {
+                text: 'Sales Orders',
+                style: {
+                    color: '#f5f7ff',
+                },
+            },
+            labels: {
+                style: {
+                    colors: ['#f5f7ff'],
+                },
+            },
+        },
+    ],
+    tooltip: {
+        shared: true,
+        intersect: false,
+        theme: 'dark',
+    },
+};
+
+const areaChart = new ApexCharts(
+    document.querySelector('#area-chart'),
+    areaChartOptions
+);
+areaChart.render();
